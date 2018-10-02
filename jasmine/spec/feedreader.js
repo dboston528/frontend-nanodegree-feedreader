ffbafa5e0f95433d81caac7affc6b31844363057
DeAndre Boston <dboston3@yahoo.com>
2018-10-01 22:57:17 -0500

    Added test for menu toggle

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -49,6 +49,15 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
 
+    describe('The menu', function(){
+
+      it('is hidden', function(){
+        const body = document.querySelector('body');
+        expect(body.classList.contains('menu-hidden')).toBe(true);
+            });
+
+    });
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
